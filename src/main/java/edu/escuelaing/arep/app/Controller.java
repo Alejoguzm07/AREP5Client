464beb286c149e6677535b06b7117ae2a627e66a
Alejoguzm07 <alejoguzm07@gmail.com>
2019-09-25 07:15:13 -0500

    Client Methods

--- src/main/java/edu/escuelaing/arep/app/Controller.java
+++ src/main/java/edu/escuelaing/arep/app/Controller.java
@@ -6,15 +6,43 @@ import java.util.ArrayList;
 
 public class Controller {
     public static void main(String[] args) throws Exception {
-        URL url1 = new URL("https://proyecto-arep1.herokuapp.com/static/index.html");
-        URL url2 = new URL("https://proyecto-arep1.herokuapp.com/static/CalculadoraWeb.html");
-        ArrayList<RequestThread> peticiones = new ArrayList<RequestThread>();
-        for(int i = 0; i < 100; i ++){
-            peticiones.add(new RequestThread(url1));
-            peticiones.add(new RequestThread(url2));
-        }
-        for(int i = 0; i < peticiones.size(); i++){
-            peticiones.get(i).start();
-        }
+    	int cantidad = 10;
+    	ArrayList<URL> links = prepararUrls(cantidad);
+    	//atacarNoConcurrentemente(links);
+    	atacarConcurrentemente(links);
+    }
+    
+    private static void atacarNoConcurrentemente(ArrayList<URL> urls) {
+    	ArrayList<RequestThread> peticiones = new ArrayList<RequestThread>();
+    	for(int i = 0; i < urls.size(); i++) {
+    		peticiones.add(new RequestThread(urls.get(i)));
+    	}
+    	for(int i = 0; i < peticiones.size(); i++) {
+    		peticiones.get(i).run();
+    	}
+    }
+    
+    private static void atacarConcurrentemente(ArrayList<URL> urls) {
+    	ArrayList<RequestThread> peticiones = new ArrayList<RequestThread>();
+    	for(int i = 0; i < urls.size(); i++) {
+    		peticiones.add(new RequestThread(urls.get(i)));
+    	}
+    	for(int i = 0; i < peticiones.size(); i++) {
+    		peticiones.get(i).start();
+    	}
+    }
+    
+    private static ArrayList<URL> prepararUrls(int num) throws MalformedURLException {
+		ArrayList<URL> links = new ArrayList<URL>();
+		ArrayList<URL> peticiones = new ArrayList<URL>();
+		links.add(new URL("https://proyecto-arep1.herokuapp.com/static/index.html"));links.add(new URL("https://proyecto-arep1.herokuapp.com/staticindex.html"));
+		links.add(new URL("https://proyecto-arep1.herokuapp.com/static/CalculadoraWeb.html"));links.add(new URL("https://proyecto-arep1.herokuapp.com/static/CalculadoraWeb.htl"));
+		links.add(new URL("https://proyecto-arep1.herokuapp.com/static/paisaje.jpg"));links.add(new URL("https://proyecto-arep1.herokuapp.com/static/paisaj.jpg"));
+		links.add(new URL("https://proyecto-arep1.herokuapp.com/apps/Division?9,3"));links.add(new URL("https://proyecto-arep1.herokuapp.com/apps/Division?9"));
+		links.add(new URL("https://proyecto-arep1.herokuapp.com/apps/Cuadrado?5"));links.add(new URL("https://proyecto-arep1.herokuapp.com/apps/Cuadrad?5"));
+		for(int i = 0; i < num; i++) {
+			peticiones.add(links.get((int)Math.random() % links.size()));
+		}
+		return peticiones;
     }
 }
